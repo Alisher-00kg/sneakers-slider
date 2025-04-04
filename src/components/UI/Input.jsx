24a2a73ca33f4eb9c905427efd04bf2a253590f0
Argen <raxmanberdievargen577@gmail.com>
2025-04-04 23:51:42 +0300

    inputsave

---
+++ src/components/UI/Input.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components";
+
+const Input = ({ type, value, onChange, placeholder, disabled, ...props }) => {
+  return (
+    <Styledinp
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      disabled={disabled}
+      {...props}
+    />
+  );
+};
+
+export default Input;
+
+const Styledinp = styled.input`
+  width: 100%;
+
+  border: 1px solid #8a8585;
+  border-radius: 4px;
+  padding-left: 10px;
+  ::placeholder {
+    font-size: 16px;
+    font-family: sans-serif;
+  }
+`;
