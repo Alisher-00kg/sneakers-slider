e0d8b8dc0ee5234971c85500b5d445a5a2f2f237
daniyar-bro <daniarakmatkulov6@gmail.com>
2025-04-04 16:33:29 +0300

    finished media

---
+++ src/assets/icons.js
@@ -0,0 +1,30 @@
+import ArrowL from "./icons/ArrowL.svg?react";
+import ArrowR from "./icons/ArrowR.svg?react";
+import Facebook from "./icons/Facebook.svg?react";
+import FiveStars from "./icons/FiveStars.svg?react";
+import FourStars from "./icons/FourStars.svg?react";
+import Heart from "./icons/Heart.svg?react";
+import Instagram from "./icons/Instagram.svg?react";
+import LogoHeader from "./icons/LogoHeader.svg?react";
+import Man from "./icons/Man.svg?react";
+import Vector from "./icons/Vector.svg?react";
+import VectorIn from "./icons/VectorIn.svg?react";
+import VectorX from "./icons/VectorX.svg?react";
+import YouTube from "./icons/VectorYouTube.svg?react";
+import Frame from "./icons/Frame.svg?react";
+export const Icons = {
+  ArrowL,
+  ArrowR,
+  Facebook,
+  FiveStars,
+  FourStars,
+  Heart,
+  Instagram,
+  LogoHeader,
+  Man,
+  Vector,
+  VectorIn,
+  VectorX,
+  YouTube,
+  Frame,
+};
