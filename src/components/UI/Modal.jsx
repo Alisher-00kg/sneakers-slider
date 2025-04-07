054cf0a25852ae0ee985f4d043a44b8f02d9a66c
daniyar-bro <daniarakmatkulov6@gmail.com>
2025-04-07 23:25:43 +0300

    newModal

---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+
+const Modal = ({ children, onClose }) => {
+  return ReactDOM.createPortal(
+    <BackDrop onClick={onClose}>
+      <ModalContent onClick={(e) => e.stopPropagation()}>
+        {children}
+      </ModalContent>
+    </BackDrop>,
+    document.body
+  );
+};
+
+const ModalContent = styled.div`
+  background: white;
+  padding: 20px;
+  border-radius: 8px;
+  width: 100px;
+  position: relative;
+`;
+
+const BackDrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1000;
+`;
+
+export default Modal;
