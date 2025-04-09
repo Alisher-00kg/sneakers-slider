e650894ee6cbaf9b98441672d72e6db2ed7a6ca4
symbat <simiabdyk@gmail.com>
2025-04-09 18:14:31 +0300

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import { PhotoNavigation } from "./components/UI/PhotoNavigation";
 
 const App = () => {
-  return <div>App
-    <PhotoNavigation/>
-  </div>;
+  return <div>App</div>;
 };
 
 export default App;
