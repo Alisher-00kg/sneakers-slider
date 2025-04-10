edfae0a67d98f9f68693c89d3dd961a40deb1f35
Kubat <Kubatb990@gmail.com>
2025-04-11 00:51:17 +0300

    create Card item

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,26 +1,42 @@
 import React from "react";
 import styled from "styled-components";
 import { Button } from "./UI/Button";
+import { Icons } from "../assets/index.js";
 
-export const CardItem = ({
-  id,
-  title,
-  newPrice,
-  oldPrice,
-  image,
-  heartImg,
-  heartST,
-}) => {
+export const CardItem = ({ id, title, newPrice, oldPrice, image, reviews }) => {
+  const functionImg = (reviews) => {
+    const style = { height: "24px" };
+    return reviews >= 88 ? (
+      <Icons.FiveStars style={style} />
+    ) : (
+      <Icons.FourStars style={style} />
+    );
+  };
+
+  const functionHeartSVG = () => {
+    return <Icons.BlackHeart />;
+  };
 
   return (
     <StyledInfoDiv key={id}>
       <StyledImgDiv>
+        <div
+          style={{
+            width: "26px",
+            height: "26px",
+            position: "relative",
+            left: "210px",
+            top: "20px",
+          }}
+        >
+          {functionHeartSVG()}
+        </div>
         <div>
           <img src={image} alt={title} />
         </div>
         <Button variant={"signin"}>Add To Carts</Button>
       </StyledImgDiv>
-      <p>{title}</p>
+      <p style={{ paddingLeft: "10px" }}>{title}</p>
       <StyledPriceInfo>
         <p
           style={{
@@ -30,25 +46,30 @@ export const CardItem = ({
           ${newPrice}
         </p>
         <OldPrice>{oldPrice >= 1160 ? "$1160" : ""}</OldPrice>
-        {}
       </StyledPriceInfo>
+      <StyledRating>
+        {functionImg(reviews)}
+        <span style={{ color: "gray" }}>{reviews}</span>
+      </StyledRating>
     </StyledInfoDiv>
   );
 };
 const StyledInfoDiv = styled.div`
+  width: 250px;
   margin: 40px;
   display: flex;
   flex-direction: column;
   gap: 20px;
   transition: 0.5s ease;
+  box-shadow: 4px 4px 10px 0px rgba(34, 60, 80, 0.2);
   h1 {
     font-size: 16px;
   }
   &:hover {
     transform: scale(1.05);
+    box-shadow: 9px 9px 8px 0px rgba(34, 60, 80, 0.2);
     button {
       display: block;
-      transition: 0.5s ease-in-out;
     }
   }
 `;
@@ -56,6 +77,7 @@ const StyledInfoDiv = styled.div`
 const StyledPriceInfo = styled.div`
   display: flex;
   gap: 12px;
+  padding-left: 10px;
 `;
 
 const OldPrice = styled.p`
@@ -64,13 +86,13 @@ const OldPrice = styled.p`
 `;
 
 const StyledImgDiv = styled.div`
-  width: 270px;
-  height: 270px;
+  width: 250px;
+  height: 220px;
   background-color: #e9e9eb;
+  box-sizing: border-box;
   img {
-    width: 200px;
-    height: 130px;
-    margin: 50px;
+    width: 250px;
+    height: 150px;
     object-fit: cover;
   }
   button {
@@ -78,5 +100,24 @@ const StyledImgDiv = styled.div`
     height: 41px;
     display: none;
     background-color: black;
+  }
+`;
+
+const StyledRating = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  padding-left: 10px;
+
+  svg {
+    height: 16px;
+    display: block;
+  }
+
+  span {
+    font-size: 16px;
+    line-height: 1;
+    position: relative;
+    top: 3px;
   }
 `;
